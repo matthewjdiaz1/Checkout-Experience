b26f6c59f3f93cf48dbb9d9443125ae46b4d2755
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-05-08 13:23:30 -0700

    localhost running with nodemon

---
+++ server/server.js
@@ -0,0 +1,10 @@
+// Build your Express app inside server.js and your client app inside the 
+// client folder.
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('dist'))
+
+app.get('/', (req, res) => res.send('Hello World, from server'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/components/app.jsx
@@ -0,0 +1,18 @@
+// For the basic requirements, you MUST place all of your React components into one file, app.jsx
+import React from 'React';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>React from App</div>
+    )
+  }
+}
+
+export default App;
